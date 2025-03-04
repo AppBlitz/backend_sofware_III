fea051098392d89615a41a89b5dc2c18e424fb8f
KJahn26 <jahn8916@gmail.com>
2025-03-03 21:57:04 -0500

    conexion a mongoDB + producto v1

---
+++ src/main/java/com/restaurant/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.restaurant.repository;
+
+import com.restaurant.model.document.Producto;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends MongoRepository<Producto,String> {
+}
