7f3f4b87b0314d98ba091d6f2c92981cea683a41
carlos <karlcorralesmovil@gmail.com>
2025-04-17 17:10:39 -0500

    r

--- src/test/java/com/restaurant/service/ProductServiceAppTest.java
+++ src/test/java/com/restaurant/service/ProductServiceAppTest.java
@@ -1,8 +1,24 @@
 package com.restaurant.service;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.restaurant.service.implementation.ProductService;
 
 @SpringBootTest
 public class ProductServiceAppTest {
+
+  @Value("${my.test.userOne}")
+  private String id;
+
+  @Autowired
+  ProductService productService;
+
+  @Test
+  public void createProductTest() {
+
+  }
 
 }
