ec97460119d437ec16812ab7da747680d9b85b29
carlos <karlcorralesmovil@gmail.com>
2025-05-24 17:38:32 -0500

    test

--- src/test/java/com/restaurant/RestaurantApplicationTests.java
+++ src/test/java/com/restaurant/RestaurantApplicationTests.java
@@ -1,30 +1,13 @@
 package com.restaurant;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestaurantApplicationTests {
 
-	private WebDriver wDriver;
-
-	@BeforeEach
-	void getUp() {
-		wDriver = new ChromeDriver();
-	}
-
 	@Test
 	void contextLoads() throws InterruptedException {
-		wDriver.get("http://localhost:5173/");
-		wDriver.findElement(By.id("email")).sendKeys("a@gmail.com");
-		wDriver.findElement(By.id("password")).sendKeys("123456789");
-		wDriver.findElement(By.id("login")).click();
-
-		wDriver.close();
 	}
 
 }
