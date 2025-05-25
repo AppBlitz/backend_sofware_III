a38c531c7661468c7eee2e4f365a2d79378ec28b
carlos <karlcorralesmovil@gmail.com>
2025-05-24 19:58:07 -0500

    perfect

--- src/test/java/com/restaurant/RestaurantApplicationTests.java
+++ src/test/java/com/restaurant/RestaurantApplicationTests.java
@@ -1,13 +1,28 @@
 package com.restaurant;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.restaurant.service.implementation.cart.ShoppinCartServiceIm;
+import com.testrail.junit.customjunitxml.annotations.TestRail;
 
 @SpringBootTest
 class RestaurantApplicationTests {
 
+	@Autowired
+	ShoppinCartServiceIm sService;
+
 	@Test
 	void contextLoads() throws InterruptedException {
+	}
+
+	@Test
+	@TestRail(id = "C35")
+	public void searchCartForId() {
+		assertNotNull(sService.searchShoppingCartId("681b61aa1e710924d1408dd6"));
 	}
 
 }
